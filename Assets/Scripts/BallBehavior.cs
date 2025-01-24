db8bb7b4e0c837cdca449aaeb2f40f00597a6a06
yousabg <greesyousab@gmail.com>
2025-01-24 15:48:48 -0600

    Jan 24 Updates
    Balls will run to the pin now!!!!

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -20,6 +20,7 @@ public class BallBehavior : MonoBehaviour
     public bool launching;
     public float launchDuration;
     public float timeLastLaunch;
+    public float timeLaunchStart;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,15 +35,32 @@ public class BallBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (onCooldown() == false) {
+            launch();
+        }
         Vector2 currentPos = gameObject.GetComponent<Transform>().position;
         float distance = Vector2.Distance(currentPos, targetPosition);
         if (distance > 0.1) {
             float difficulty = getDifficultyPercentage();
-            float currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            float currentSpeed;
+            if (launching == true) {
+                float launchingForHowLong = Time.time - timeLaunchStart;
+                if (launchingForHowLong > launchDuration) {
+                    startCooldown();
+                }
+                currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed, difficulty);
+            } else {
+                currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            }
+
             currentSpeed = currentSpeed * Time.deltaTime;
             Vector2 newPosition = Vector2.MoveTowards(currentPos, targetPosition, currentSpeed);
             transform.position = newPosition;
         } else {
+            if (launching == true) {
+                startCooldown();
+            }
             targetPosition = getRandomPosition();
         }
     }
@@ -61,6 +79,28 @@ public class BallBehavior : MonoBehaviour
     }
 
     public void launch() {
-        
+        targetPosition = target.transform.position;
+
+        if (launching == false) {
+            timeLaunchStart = Time.time;
+            launching = true;
+        }
+    }
+
+    public bool onCooldown() {
+        bool result = false;
+
+        float timeSinceLastLaunch = Time.time - timeLastLaunch;
+
+        if (timeSinceLastLaunch < cooldown) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public void startCooldown() {
+        timeLastLaunch = Time.time;
+        launching = false;
     }
 }
