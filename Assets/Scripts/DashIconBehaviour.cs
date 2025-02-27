c5762b4f56badf5c18dc036ca36adaf576c14ab2
yousabg <greesyousab@gmail.com>
2025-02-27 15:59:04 -0600

    Fixing overlay on invincibility

--- Assets/Scripts/DashIconBehaviour.cs
+++ Assets/Scripts/DashIconBehaviour.cs
@@ -7,14 +7,18 @@ public class DashIconBehaviour : MonoBehaviour
     TextMeshProUGUI label;
     float cooldown;
     float cooldownRate;
+    float dashCooldownRate;
     Image overlay;
     void Start()
     {
         label = GetComponentInChildren<TextMeshProUGUI>();
-        cooldownRate = PinBehaviour.cooldownRate;   
+        cooldownRate = PinBehaviour.cooldownRate;
+        dashCooldownRate = PinBehaviour.invincibilityCooldownRate;
         Image[] images = GetComponentsInChildren<Image>();
-        for (int i = 0; i < images.Length; i++) {
-            if (images[i].tag == "overlay") {
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (images[i].tag == "overlay")
+            {
                 overlay = images[i];
             }
         }
@@ -23,17 +27,29 @@ public class DashIconBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.tag == "Dash") {
+        if (gameObject.tag == "Dash")
+        {
             cooldown = PinBehaviour.cooldown;
-        } else if (gameObject.tag == "Invincibility") {
+            string message = "";
+            if (cooldown > 0.0)
+            {
+                float fill = cooldown / cooldownRate;
+                message = string.Format("{0:0.0}", cooldown);
+                overlay.fillAmount = fill;
+            }
+            label.SetText(message);
+        }
+        else if (gameObject.tag == "Invincibility")
+        {
             cooldown = PinBehaviour.invincibilityCooldown;
+            string message = "";
+            if (cooldown > 0.0)
+            {
+                float fill = cooldown / dashCooldownRate;
+                message = string.Format("{0:0.0}", cooldown);
+                overlay.fillAmount = fill;
+            }
+            label.SetText(message);
         }
-        string message = "";
-        if (cooldown > 0.0) {
-            float fill = cooldown / cooldownRate;
-            message = string.Format("{0:0.0}", cooldown);
-            overlay.fillAmount = fill;
-        }
-        label.SetText(message);
     }
 }
